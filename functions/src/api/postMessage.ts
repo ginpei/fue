11df718839a7387533fdf55ac0911104c5e9bf5c
ginpei <ginpei@ginpei.dev>
2022-06-20 13:09:02 -0700

    feat: 🎸 http error

--- functions/src/api/postMessage.ts
+++ functions/src/api/postMessage.ts
@@ -2,26 +2,63 @@ import {logger} from "firebase-functions/v1";
 import {onRequest} from "firebase-functions/v1/https";
 import {createMessage, Message} from "../../../src/domains/messages/Message";
 import {getFirestore} from "../firebase";
+import {HttpError} from "../tools/httpError";
 import {getRequestBody} from "../tools/request";
 
 export const postMessage = onRequest(async (req, res) => {
-  logger.info("postMessage", {
-    body: req.body,
-    ip: req.ip,
-  });
+  try {
+    logger.info("postMessage", {
+      body: req.body,
+      ip: req.ip,
+    });
 
-  // TODO validate
-  const body = getRequestBody<Message>(req);
-  const message = createMessage({...body, ip: req.ip});
+    // TODO validate
+    const body = getRequestBody<Message>(req);
+    const message = createMessage({...body, ip: req.ip});
 
-  // TODO collection
-  const db = getFirestore();
-  const coll = db.collection("api");
-  const ref = await coll.add(message);
+    // TODO collection
+    const db = getFirestore();
+    const coll = db.collection("api");
+    const ref = await coll.add(message);
 
-  const ss = await ref.get();
-  const storedData = {...ss.data(), id: ss.id};
+    const ss = await ref.get();
+    const storedData = {...ss.data(), id: ss.id};
 
-  // TODO CORS
-  res.json(storedData);
+    // TODO CORS
+    res.json(storedData);
+  } catch (error) {
+    if (error instanceof HttpError) {
+      logger.error({
+        error: {
+          code: error.code,
+          message: error.message,
+          stack: error.stack,
+        },
+      });
+
+      res.sendStatus(error.code);
+      res.end();
+
+      return;
+    } else if (error instanceof Error) {
+      logger.error({
+        error: {
+          message: error.message,
+          stack: error.stack,
+        },
+      });
+
+      res.sendStatus(500);
+      res.end();
+
+      return;
+    }
+
+    logger.error({
+      error: String(error),
+    });
+
+    res.sendStatus(500);
+    res.end();
+  }
 });
--- functions/src/tools/request.ts
+++ functions/src/tools/request.ts
@@ -1,11 +1,13 @@
 import {Request} from "firebase-functions/v1";
+import {HttpError} from "./httpError";
 
 export function getRequestBody<T>(req: Request): Partial<T> {
   try {
     return JSON.parse(req.body);
   } catch (error) {
-    const httpError = new Error(
-        `400 Failed to parse request body (JSON expected)\n${req.body}`
+    const httpError = new HttpError(
+        400,
+        `Failed to parse request body (JSON expected)\n${req.body}`
     );
     throw httpError;
   }
