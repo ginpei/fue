4e5a3b773271a6b9ddbcc689cab83f0a11c1e712
ginpei <ginpei@ginpei.dev>
2022-06-20 21:22:15 -0700

    feat: 🎸 log validation errors

--- functions/src/tools/errors.ts
+++ functions/src/tools/errors.ts
@@ -1,3 +1,4 @@
+import {ValidationErrorGroup} from "../../../src/domains/errors/ValidationErrorGroup";
 import {HttpError} from "./httpError";
 
 export function buildErrorLogContent(error: unknown): Record<string, unknown> {
@@ -5,6 +6,14 @@ export function buildErrorLogContent(error: unknown): Record<string, unknown> {
     return {
       error: {
         code: error.code,
+        message: error.message,
+        stack: error.stack,
+      },
+    };
+  } else if (error instanceof ValidationErrorGroup) {
+    return {
+      error: {
+        errors: error.messages,
         message: error.message,
         stack: error.stack,
       },
