bd4a7c09a8e15cc77f291ffccdd65f8b03bb3807
Ginpei <ginpei@ginpei.dev>
2022-06-23 19:38:36 +0000

    feat: 🎸 show dashboard after login

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import { BasicLayout } from "../../layouts/basic/BasicLayout";
-import { homePagePath } from "../home/homePageMeta";
+import { dashboardPagePath } from "../dashboard/dashboardPageMeta";
 import { LoginForm } from "./LoginForm";
 
 export interface LoginPageProps {
@@ -11,7 +11,7 @@ export function LoginPage(): JSX.Element {
   const router = useRouter();
 
   const onLoggedIn = () => {
-    router.push(homePagePath());
+    router.push(dashboardPagePath());
   };
 
   return (
