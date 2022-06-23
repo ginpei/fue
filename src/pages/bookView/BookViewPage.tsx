11af9782e456d934290f913cf6a222e4b5f1678f
ginpei <ginpei@ginpei.dev>
2022-06-23 00:13:33 -0700

    feat: 🎸 dev tool to populate messages

--- src/pages/bookView/BookViewPage.tsx
+++ src/pages/bookView/BookViewPage.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { ChangeEventHandler, FormEventHandler, useState } from "react";
 import { useBook } from "../../domains/books/bookHooks";
+import { createMessage } from "../../domains/messages/Message";
 import { useBookMessages } from "../../domains/messages/messageHooks";
 import { BasicLayout } from "../../layouts/basic/BasicLayout";
 import { working } from "../../misc/working";
+import { InputField } from "../../ui/forms/InputField";
+import { PrimaryButton } from "../../ui/forms/NiceButton";
+import { TextField } from "../../ui/forms/TextField";
 import { ErrorMessage } from "../../ui/util/ErrorMessage";
 import { LiningText } from "../../ui/util/LiningText";
+import { VStack } from "../../ui/util/VStack";
 import { bookEditPagePath } from "../bookEdit/bookEditPageMeta";
 import { LoadingPage } from "../loading/LoadingPage";
 import { NotFoundPage } from "../notFound/NotFoundPage";
@@ -38,6 +44,7 @@ export function BookViewPage(): JSX.Element {
       {messages.map((message) => (
         <MessageItem key={message.id} message={message} />
       ))}
+      {bookId && <DevPostMessageSection bookId={bookId} />}
     </BasicLayout>
   );
 }
@@ -54,4 +61,73 @@ function useRouterBookId(): string | working {
   }
 
   return bookId;
+}
+
+function DevPostMessageSection(props: { bookId: string }): JSX.Element {
+  const initialMessage = createMessage({
+    bookId: props.bookId,
+    url: "https://example.com/path/to/content",
+  });
+  const [message, setMessage] = useState(initialMessage);
+
+  const onSubmit: FormEventHandler = async (event) => {
+    event.preventDefault();
+    const res = await fetch("http://127.0.0.1:5001/ginpei-fue/us-central1/postMessage", {
+      body: JSON.stringify(message),
+      method: "POST",
+    });
+    const data = await res.json();
+    console.log('# data', data);
+    setMessage(initialMessage);
+  };
+
+  const onChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> = (event) => {
+    const { name, value } = event.currentTarget;
+    if (name === "body") {
+      setMessage({ ...message, body: value });
+    } else {
+      throw new Error(`Unknown name: ${name}`);
+    }
+  };
+
+  return (
+    <VStack>
+      <h2>[DEV] Post messages</h2>
+      <form onSubmit={onSubmit}>
+        <VStack>
+          <TextField
+            label="Body"
+            name="body"
+            onChange={onChange}
+            value={message.body}
+          />
+          <InputField
+            label="Book ID"
+            name="bookId"
+            readOnly
+            value={message.bookId}
+          />
+          <InputField
+            label="Quote"
+            name="quote"
+            onChange={onChange}
+            value={message.quote}
+          />
+          <InputField
+            label="Quote path"
+            name="quotePath"
+            onChange={onChange}
+            value={message.quotePath}
+          />
+          <InputField
+            label="URL"
+            name="url"
+            onChange={onChange}
+            value={message.url}
+          />
+          <PrimaryButton>Post</PrimaryButton>
+        </VStack>
+      </form>
+    </VStack>
+  );
 }
